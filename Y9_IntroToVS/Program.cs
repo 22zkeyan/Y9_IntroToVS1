f0b93a0e450b695af76df36f99dac92e6c5e3c40
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-01-07 12:01:57 +0000

    Add project files.

---
+++ Y9_IntroToVS/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Y9_IntroToVS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho, World!");
+            Console.WriteLine("Helld!");
+        }
+    }
+}
